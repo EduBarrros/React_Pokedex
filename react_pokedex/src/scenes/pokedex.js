fae234da8d5371684855b1f45924f48b4da7683e
EduBarrros <eduardodeoliveirabarros@gmail.com>
2022-02-05 18:46:55 -0300

    Create pokemonCard components and AppBar

--- react_pokedex/src/scenes/pokedex.js
+++ react_pokedex/src/scenes/pokedex.js
@@ -1,10 +1,43 @@
 import React from "react";
+import { AppBar, Toolbar, Grid, Card, CardContent } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    pokedexContainer: {
+        paddingTop: "20px",
+        paddingLeft: "50px",
+        paddingRight: "50px"
+    },
+});
+
+const getPokemonCards = () => {
+    return (
+        <Grid item xs={4}>
+            <Card>
+                <CardContent>
+                    hi
+                </CardContent>
+            </Card>
+        </Grid>
+    )
+}
+
 
 const Pokedex = () => {
+    const classes = useStyles();
     return (
-        <div>
-            Essa é a pagina da Pokedex!
-        </div>
+        <>
+            <AppBar position="static">
+                <Toolbar />
+            </AppBar>
+            <Grid container spacing={2} className={classes.pokedexContainer}>
+                {getPokemonCards()}
+                {getPokemonCards()}
+                {getPokemonCards()}
+                {getPokemonCards()}
+                {getPokemonCards()}
+            </Grid>
+        </>
     )
 }
 
