8c911a02deeceec51b07244850535f5af0ddc494
EduBarrros <74619318+EduBarrros@users.noreply.github.com>
2022-02-06 14:31:30 -0300

    Finalização da configuração com os dados mockados

--- react_pokedex/src/scenes/pokemon.js
+++ react_pokedex/src/scenes/pokemon.js
@@ -1,16 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
+import MockData from "../mockData";
+import { Typography, Link } from '@material-ui/core';
+import { toFirstCharUppercase } from "../constants";
 
 const Pokemon = () => {
 
-    const pokemonId  = useParams();
+    const pokemonId = useParams();
+    const [pokemon, setPokemon] = useState(MockData[`${pokemonId.pokemonId}`])
 
     console.log(pokemonId)
-    return(
-        <div>
-            {`Essa é a pagina do Pokemon de número: ${pokemonId.pokemonId}`}
-        </div>
-    )
+    console.log(pokemon)
+
+    const generatePokemonJsx = () => {
+        const { name, id, species, height, weight, types, sprites } = pokemon;
+        console.log(id)
+        const fullImageUrl = `https://pokeres.bastionbot.org/images/pokemon/${id}.png`;
+        const { front_default } = sprites;
+
+        return (
+            <>
+                <Typography variant="h1">
+                    {`${id}.`} {toFirstCharUppercase(name)}
+                    <img src={front_default} />
+                </Typography>
+                <img style={{ width: "300px", height: "300px" }} src={front_default} />
+                <Typography variant="h3">Pokemon Info</Typography>
+                <Typography>
+                    {"Species: "}
+                    <Link href={species.url}>{species.name} </Link>
+                </Typography>
+                <Typography>Height: {height} </Typography>
+                <Typography>Weight: {weight} </Typography>
+                <Typography variant="h6"> Types:</Typography>
+                {types.map((typeInfo) => {
+                    const { type } = typeInfo;
+                    const { name } = type;
+                    return <Typography key={name}> {`${name}`}</Typography>;
+                })}
+            </>
+        )
+    }
+
+    return (
+        <>
+            {generatePokemonJsx()}
+        </>
+    );
 }
 
 export default Pokemon;
